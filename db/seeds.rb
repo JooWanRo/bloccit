c29bfc6d37efff7b34c0c9e11d9edf1a36448d31
Joo Wan Ro <ro.joowan@gmail.com>
2015-11-16 17:15:09 -0500

    Assignment 32 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 require 'faker'
 
+init_post_size = Post.count
+init_comment_size = Comment.count
+
+=begin
 # Create Posts
 50.times do
   Post.create!(
@@ -20,3 +24,31 @@ end
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+=end
+
+# Create a unique Post
+# title: Unique Title
+# body:  HELLO!
+if ((Post.find_by title: 'Unique Title', body: 'HELLO!') == nil) then
+  Post.create!(
+    title: 'Unique Title',
+    body:  'HELLO!'
+  )
+end
+
+# This post had id: 51
+post = Post.find_by title: 'Unique Title', body: 'HELLO!'
+
+# Create a unique Comment
+# post_id: 51
+# body:    COMMENT!
+if ((Comment.find_by post: post, body: 'COMMENT!') == nil) then
+  Comment.create!(
+    post: post,
+    body: 'COMMENT!'
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count - init_post_size} posts created"
+puts "#{Comment.count - init_comment_size} comments created"
