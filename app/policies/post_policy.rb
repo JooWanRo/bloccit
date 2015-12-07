1733c1efcd3e5d070c1e7f541f237dab0677fd3d
Joo Wan Ro <ro.joowan@gmail.com>
2015-12-07 17:28:03 -0500

    Assignment 39 completed

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,2 +1,13 @@
 class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if !user.present?
+        scope.where(:id => 0)
+      elsif user.admin? || user.moderator?
+        scope.all
+      else
+        scope.where(:user_id => user.id)
+      end
+    end
+  end
 end
