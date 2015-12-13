ea3d77812b301f0e131767c3dd27721537e37bc2
Joo Wan Ro <ro.joowan@gmail.com>
2015-12-12 21:34:25 -0500

    Checkpoint 40 TopicsAndPosts: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  resources :posts
+
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   # /welcome/about linked to /about
   get 'about' => 'welcome#about'
