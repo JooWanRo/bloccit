a60fbe1c9bebf994cb07b7086b8a7d410b398276
Joo Wan Ro <ro.joowan@gmail.com>
2015-12-27 20:00:43 -0500

    Checkpoint 49 Voting: completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe Post do
   describe "vote methods" do
     before do
-      @post = Post.create(title: 'post title', body: 'post body')
+      @post = Post.create(title: 'Post title', body: 'Post bodiest be pretty long.')
       3.times { @post.votes.create(value: 1) }
-      2.times { @post.votes.create(value: -1) }
+      1.times { @post.votes.create(value: -1) }
     end
 
     describe '#up_votes' do
@@ -16,13 +16,13 @@ describe Post do
 
     describe '#down_votes' do
       it "counts the number of votes with value = -1" do
-        expect( @post.down_votes ).to eq(2)
+        expect( @post.down_votes ).to eq(1)
       end
     end
 
     describe '#points' do
       it "returns the sum of all down and up votes" do
-        expect( @post.points ).to eq(1)
+        expect( @post.points ).to eq(2)
       end
     end
   end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 describe Vote do
   describe "validations" do
     before do
-      @good_vote_1 = Vote.create(value: 1)
-      @good_vote_2 = Vote.create(value: -1)
-      @bad_vote_1 = Vote.create(value: 0)
-      @bad_vote_2 = Vote.create(value: 10)
-      @bad_vote_3 = Vote.create(value: -10)
+      @good_vote_1 = Vote.new(value: 1)
+      @good_vote_2 = Vote.new(value: -1)
+      @bad_vote_1 = Vote.new(value: 0)
+      @bad_vote_2 = Vote.new(value: 10)
+      @bad_vote_3 = Vote.new(value: -10)
     end
 
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect( @good_vote_1.save ).to eq(true)
-        expect( @good_vote_2.save ).to eq(true)
-        expect( @bad_vote_1.save ).to eq(false)
-        expect( @bad_vote_2.save ).to eq(false)
-        expect( @bad_vote_3.save ).to eq(false)
+        expect( @good_vote_1.valid? ).to eq(true)
+        expect( @good_vote_2.valid? ).to eq(true)
+        expect( @bad_vote_1.valid? ).to eq(false)
+        expect( @bad_vote_2.valid? ).to eq(false)
+        expect( @bad_vote_3.valid? ).to eq(false)
       end
     end
   end
