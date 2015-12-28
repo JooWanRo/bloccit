0ed6fe9b5bff434a0e6e2b9bd2dbfa2bd056136a
Joo Wan Ro <ro.joowan@gmail.com>
2015-12-27 20:24:08 -0500

    Assignment 49 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,7 +38,7 @@ topics = Topic.all
   )
 
   # set the created_at to a time within the past year
-  post.update_attributres!(created_at: rand(10.minutes .. 1.year).ago)
+  post.update_attributes!(created_at: rand(10.minutes .. 1.year).ago)
   post.update_rank
 end
 posts = Post.all
