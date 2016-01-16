89769496cd6cafad7e18d79cd580ceb846093f6f
Joo Wan Ro <ro.joowan@gmail.com>
2016-01-16 17:41:53 -0500

    Checkpoint 58 CreatingWithAJAX: completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,24 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(comment_params)
+    @comments = @post.comments
+
+    @comment = Comment.new( comment_params )
     @comment.user = current_user
     @comment.post = @post
+    @new_comment = Comment.new
+
     authorize @comment
 
     if @comment.save
-      flash[:notice] = "Comment saved successfully."
-      redirect_to [@post.topic, @post]
+      flash[:notice] = "Comment was created."
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to [@post.topic, @post]
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
@@ -24,13 +31,15 @@ class CommentsController < ApplicationController
       flash[:notice] = "Comment was removed."
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-    end 
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
-  respond_to do |format|
-    format.html
-    format.js
-  end
+  private
 
   def comment_params
     params.require(:comment).permit(:body)
