95448a895708892c372ae7607665e894af15b7d2
Joo Wan Ro <ro.joowan@gmail.com>
2016-01-16 16:23:10 -0500

    Assignment 56 completed

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,6 +5,17 @@ FactoryGirl.define do
     password "helloworld"
     password_confirmation "helloworld"
     confirmed_at Time.now
+
+    factory :user_with_post_and_comment do
+      ignore do
+        comments_count 1
+      end
+
+      after(:create) do |user, evaluator|
+        post = create(:post, user: user)
+        evaluator.comments_count.times { create(:comment, user: user, post: post) }
+      end
+    end
   end
 end
 
